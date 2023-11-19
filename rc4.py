2796a770923575b0a79bd688f73021ae05a35e22
TheGhossst <nandukrishnapkm97@gmail.com>
2023-11-19 19:31:04 +0530

    add function

--- rc4.py
+++ rc4.py
@@ -1,6 +1,15 @@
+def rc4_key_scheduling(key):
+    
+    
+    
 def main():
     key = input("Enter the key as a string of ASCII characters : ")
     data = input("Enter the data to encrypt as a string of ASCII characters: ")
 
     key_bytes = [ord(char) for char in key]
     data_bytes = [ord(char) for char in data]
+    
+    steps = []
+
+    key_scheduling = rc4_key_scheduling(key_bytes)
+    steps.append(key_scheduling.copy())
