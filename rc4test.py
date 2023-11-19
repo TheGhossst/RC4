8283c6b868bf351b78ed9c845b531841a83ffaf0
TheGhossst <nandukrishnapkm97@gmail.com>
2023-11-19 19:59:59 +0530

    add function detail

--- rc4test.py
+++ rc4test.py
@@ -1,5 +1,18 @@
 def rc4(key, plaintext):
-    
+    #KSA
+    S = list(range(4))
+    i=0
+    j = 0
+    T=[1,2,3,6]
+    print("Initial State (S):", S)
+    print("T = ",T)
+    for i in range(0,4,1):
+        j = (j + S[i] + T[i]) % 4
+        print("Value of S[i] and T[i] is ",S[i],T[i])
+        S[i], S[j] = S[j], S[i]
+        print("Value of j is ",j)
+        print(f"After iteration {i+1} (KSA):", S)
+    print("\n\n")
     
     
     
